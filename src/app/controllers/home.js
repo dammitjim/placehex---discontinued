f74972634b5096a0d55128477df0f84ead13c7c0
Jim Hill <jimi2204@googlemail.com>
2015-09-03 22:31:22 +0100

    Refactoring and making text colour adapt to hex values

--- src/app/controllers/home.js
+++ src/app/controllers/home.js
@@ -1,8 +1,9 @@
 var express = require('express'),
-  router = express.Router(),
-  mongoose = require('mongoose'),
-  webshot = require('webshot'),
-  path = require('path');
+router = express.Router(),
+mongoose = require('mongoose'),
+webshot = require('webshot'),
+path = require('path'),
+fs = require('fs');
 
 import Generator from '../lib/generator';
 
@@ -10,41 +11,60 @@ module.exports = function (app) {
   app.use('/', router);
 };
 
-router.get('/', (req, res, next) => {
-  res.render('index');
-});
-
 /**
- * Renders the html to an image and passes the filename to the callback
- * @param  Generator() generator
- * @param  Array       args      - .size .hex .text .output (HTML string)
- * @param  Function    callback  - (filename)
- * @return void
- */
+* Renders the html to an image and passes the filename to the callback
+* @param  Generator() generator
+* @param  Array       args      - .size .hex .text .output (HTML string)
+* @param  Function    callback  - (filename)
+* @return void
+*/
 function generateOutput(generator, args, callback) {
-    // Generate filename
-    let filename = args.size + '_' + args.hex + '_' + Date.now();
-    // Get output path
-    let filepath = path.normalize(__dirname + '/../../..') + '/public/img/output/' + filename + '.png';
-    // node_webshot
-    webshot(args.output, filepath, {
-      siteType: 'html',
-      windowSize: {
-        width: generator.width,
-        height: generator.height
-      }
-     }, (err) => {
-       callback(filename);
-    });
+  // Generate filename
+  let filename = generator.getFilename();
+  // Get output path
+  let filepath = generator.getFilepath();
+  // node_webshot
+  webshot(args.output, filepath, {
+    siteType: 'html',
+    windowSize: {
+      width: generator.width,
+      height: generator.height
+    }
+  }, (err) => {
+    callback(filepath);
+  });
 }
+
+function randomNumber(limit = 999) {
+  return Math.ceil(Math.random()*limit);
+}
+
+function randomHex() {
+  return Math.floor(Math.random()*16777215).toString(16);
+}
+
+function generateRandomURL() {
+  let size = randomNumber() + 'x' + randomNumber();
+  let colour = randomHex();
+  let text = 'Test';
+  return [size, colour, text].join('/');
+}
+
+router.get('/', (req, res, next) => {
+  let args = [];
+  args.url = generateRandomURL();
+  res.render('index', args);
+});
 
 router.get('/:size/:hex/:text', (req, res, next) => {
   let args = req.params;
   let generator = new Generator(args.size, args.hex, args.text);
   res.render('placeholder', generator, (err, placeholder) => {
     args.output = placeholder;
-    generateOutput(generator, args, (filename) => {
-       res.render('output', { filename: filename });
+    generateOutput(generator, args, (filepath) => {
+      var img = fs.readFileSync(filepath);
+      res.writeHead(200, {'Content-Type': 'image/gif' });
+      res.end(img, 'binary');
     });
   });
 });
@@ -54,8 +74,10 @@ router.get('/:size/:hex', (req, res, next) => {
   let generator = new Generator(args.size, args.hex);
   res.render('placeholder', generator, (err, placeholder) => {
     args.output = placeholder;
-    generateOutput(generator, args, (filename) => {
-       res.render('output', { filename: filename });
+    generateOutput(generator, args, (filepath) => {
+      var img = fs.readFileSync(filepath);
+      res.writeHead(200, {'Content-Type': 'image/gif' });
+      res.end(img, 'binary');
     });
   });
 });
@@ -65,8 +87,10 @@ router.get('/:size', (req, res, next) => {
   let generator = new Generator(args.size);
   res.render('placeholder', generator, (err, placeholder) => {
     args.output = placeholder;
-    generateOutput(generator, args, (filename) => {
-       res.render('output', { filename: filename });
+    generateOutput(generator, args, (filepath) => {
+      var img = fs.readFileSync(filepath);
+      res.writeHead(200, {'Content-Type': 'image/gif' });
+      res.end(img, 'binary');
     });
   });
 });
--- src/app/lib/generator.js
+++ src/app/lib/generator.js
@@ -1,18 +1,37 @@
+let path = require('path');
+
 export default class Placeholder {
+
   constructor(size, hex = null, text = null) {
     let sizes = size.split('x');
     this.width = sizes[0];
     this.height = sizes[1];
-    this.colour = '#' + hex;
-    this.textColour = '#' + this.lightenColour(20);
+    this.colour = hex;
+    this.textColour = this.getContrastYIQ(this.colour);
+    if (typeof text == 'undefined') {
+      text = [sizes[0], sizes[1]].join('x');
+    }
     this.text = text;
   }
-  lightenColour(percent) {
-    var num = parseInt(this.color,16),
-    amt = Math.round(2.55 * percent),
-    R = (num >> 16) + amt,
-    G = (num >> 8 & 0x00FF) + amt,
-    B = (num & 0x0000FF) + amt;
-    return (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (G<255?G<1?0:G:255)*0x100 + (B<255?B<1?0:B:255)).toString(16).slice(1);
+
+  /**
+   * From https://24ways.org/2010/calculating-color-contrast
+   * @param  string hexcolor
+   * @return 000000 || ffffff
+   */
+  getContrastYIQ(hexcolor) {
+    var r = parseInt(hexcolor.substr(0,2),16);
+    var g = parseInt(hexcolor.substr(2,2),16);
+    var b = parseInt(hexcolor.substr(4,2),16);
+    var yiq = ((r*299)+(g*587)+(b*114))/1000;
+    return (yiq >= 128) ? '000000' : 'ffffff';
+  }
+
+  getFilename() {
+    return [this.width + 'x' + this.height, this.colour, Date.now()].join('_') + '.png';
+  }
+
+  getFilepath() {
+    return path.normalize(__dirname + '/../../..') + '/public/img/output/' + this.getFilename();
   }
 }
